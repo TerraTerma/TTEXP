3d6d50d1fb3604aea17328ec4464fbf400e3bd2e
JustMaffie Development <itisjori@gmail.com>
2016-08-12 18:47:41 +0200

    First things added.

---
+++ src/me/JustMaffie/TerraTerma/TTEXP/API/EXPAPI.java
@@ -0,0 +1,13 @@
+package me.JustMaffie.TerraTerma.TTEXP.API;
+
+import me.JustMaffie.TerraTerma.TTEXP.Main.TTEXP;
+
+public class EXPAPI {
+	private static TTEXP ttexp;
+	
+	
+	public static TTEXP getMainPlugin(){
+
+		return ttexp;
+	}
+}
---
+++ src/me/JustMaffie/TerraTerma/TTEXP/Main/TTEXP.java
@@ -0,0 +1,19 @@
+package me.JustMaffie.TerraTerma.TTEXP.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TTEXP extends JavaPlugin{
+	
+	public void onEnable(){
+		
+		
+	}
+	
+	public void onDisable(){
+		
+		
+	}
+
+	
+
+}
